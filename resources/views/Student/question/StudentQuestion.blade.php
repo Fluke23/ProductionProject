6b39717533747957adf6fe1c123e471c6f46e0ba
Fluke23 <piya.dec@mail.kmutt.ac.th>
2018-10-27 15:38:16 +0700

    [edit]review status student cannot access

--- resources/views/Student/question/StudentQuestion.blade.php
+++ resources/views/Student/question/StudentQuestion.blade.php
@@ -4,21 +4,29 @@
 <div class="container">
     <div class="row mb-2">
         <div class="col-md-6">
-            <h2 >Question Manager</h2>
-           
-        </div>
-            
-                 
-  </div>
-  <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="{{ URL::to('/Student/subject')}}">Home</a></li>
-              <li class="breadcrumb-item"><a href="{{ URL::to('/Student/quiz/StudentquizDetail')}}">Quiz Manager</a></li>
-              <li class="breadcrumb-item"><a href="{{ URL::to('/Student/question/StudentQuestion')}}">Question Manager</a></li>
-            </ol>
-          </nav>
+            <h2>Question Manager</h2>
 
-<div class="row">
+        </div>
+
+
+    </div>
+    @if(Session::has('unsuccess'))
+    <div class="alert alert-danger">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <strong>Cannot access because Lecturer still reviewing</strong> {{ Session::get('message', '') }}
+    </div>
+    @endif
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{ URL::to('/Student/subject')}}">Home</a></li>
+            <li class="breadcrumb-item"><a href="{{ URL::to('/Student/quiz/StudentquizDetail')}}">Quiz Manager</a></li>
+            <li class="breadcrumb-item"><a href="{{ URL::to('/Student/question/StudentQuestion')}}">Question Manager</a></li>
+        </ol>
+    </nav>
+
+
+
+    <div class="row">
         <table class="table table-bordered">
             <tr>
                 <th style="font-size: 1em;">Question</th>
@@ -30,38 +38,39 @@
                 <th style="width:50px;">max</th>
                 <th style="width:50px;">Avg</th>
                 <th></th>
-                
+
 
             </tr>
 
             <tbody>
-                    @foreach($question as $que)
+                @foreach($question as $que)
                 <tr>
-                        <td style="font-size: 0.8em;">{{$que->question}}</td>
-                        <td style="font-size: 0.8em;">{{$que->score}}</td>
-                        <td style="font-size: 0.8em;">Tester</td>
-                        <td style="font-size: 0.8em;">min</td>
-                        <td style="font-size: 0.8em;">max</td>
-                        <td style="font-size: 0.8em;">Avg</td>
-                
-                        
-                    
+                    <td style="font-size: 0.8em;">{{$que->question}}</td>
+                    <td style="font-size: 0.8em;">{{$que->score}}</td>
+                    <td style="font-size: 0.8em;">Tester</td>
+                    <td style="font-size: 0.8em;">min</td>
+                    <td style="font-size: 0.8em;">max</td>
+                    <td style="font-size: 0.8em;">Avg</td>
 
-                        <td >
-                        <a href="{{URL::route('AnswerBlankQuestion.file').'/'.$que->questions_id.'/'.$que->quizs_id}}" class="btn btn-info ">View</a>
-                        </td>
+
+
+
+                    <td>
+                        <a href="{{URL::route('AnswerBlankQuestion.file').'/'.$que->questions_id.'/'.$que->quizs_id}}"
+                            class="btn btn-info ">View</a>
+                    </td>
                 </tr>
-                     @endforeach
+                @endforeach
             </tbody>
- 
+
         </table>
-        
-         
-         <hr>
+
+
+        <hr>
     </div>
-               
-    </div>
-    
+
+</div>
+
 
 
 </div>
