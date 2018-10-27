3d074f09e8fae70b436d48d3cc0b7a40c848c95e
Fluke23 <piya.dec@mail.kmutt.ac.th>
2018-10-27 17:48:52 +0700

    [edit]score

--- resources/views/Admin/question/MultipleChoice.blade.php
+++ resources/views/Admin/question/MultipleChoice.blade.php
@@ -52,7 +52,7 @@
 
             <div class="form-group">
                 {{Form::label('score'.$i, 'score')}}
-                {{Form::text('score'.$i, '',['placeholder'=> 'Enter Score'])}}
+                {{Form::text('score'.$i, '1')}}
             </div>
 
             <div class="form-group">
