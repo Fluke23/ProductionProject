469f41fcd58141f1052938ef5a818ca1281c7869
Fluke23 <piya.dec@mail.kmutt.ac.th>
2018-10-29 22:43:33 +0700

    [edit]min max avg question

--- resources/views/Admin/question/index.blade.php
+++ resources/views/Admin/question/index.blade.php
@@ -59,9 +59,9 @@
                         <td style="font-size: 0.8em;">{{$q->question}}</td>
                         <td style="font-size: 0.8em;">{{$q->score}}</td>
                         <td style="font-size: 0.8em;">{{$q->questions_types_id}}</td>
-                        <td style="font-size: 0.8em;">{{$question_min}}</td>
-                        <td style="font-size: 0.8em;">{{$question_max}}</td>
-                        <td style="font-size: 0.8em;">{{$question_avg}}</td>
+                        <td style="font-size: 0.8em;">{{$q->min}}</td>
+                        <td style="font-size: 0.8em;">{{$q->max}}</td>
+                        <td style="font-size: 0.8em;">{{$q->avg}}</td>
 
                         <td >
                         <a href="{{URL::to('/Admin/checkAnswer/indexAnswer/'.$q->questions_id)}}" class="btn btn-info ">View</a>
