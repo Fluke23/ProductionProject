a84e309d61a187a9341109662c0e6bb8b81f8159
Fluke23 <piya.dec@mail.kmutt.ac.th>
2018-11-08 19:30:30 +0700

    [edit]group g1 g2

--- app/Quiz.php
+++ app/Quiz.php
@@ -9,7 +9,7 @@ class Quiz extends Model
     protected $table = 'quizs';
     public $timestamps = false;
     public $primaryKey = 'quizs_id';
-    protected $fillable = ['title','description','subject_id','groups_id','quizs_types_id','quizs_status_id','quiz_date'];
+    protected $fillable = ['title','description','subject_id','student_group','quizs_types_id','quizs_status_id','quiz_date'];
 
     // public function quiz_status(){
     //     return $this->belongsTo('App\Quiz_status');
